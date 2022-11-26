ef7832ddf891f85c871f00e39ef4df029b6a6272
Pero Maric <peromaric91@gmail.com>
2022-11-26 18:08:53 +0100

    refactor grade input again

--- src/main/java/hr/java/vjezbe/entitet/Unos.java
+++ src/main/java/hr/java/vjezbe/entitet/Unos.java
@@ -17,31 +17,21 @@ public interface Unos {
     }
 
     static Ocjena unosOcjene(Scanner scanner) {
-        System.out.print("Unesite ocjenu na ispitu (1-5): ");
-        int ocjenaUnos = Integer.parseInt(scanner.nextLine());
+        try {
+            System.out.print("Unesite ocjenu na ispitu (1-5): ");
+            int ocjenaUnos = Integer.parseInt(scanner.nextLine());
 
-        Ocjena ocjena = null;
-        switch (ocjenaUnos) {
-            case 1:
-                ocjena = Ocjena.JEDAN;
-                break;
-            case 2:
-                ocjena = Ocjena.DVA;
-                break;
-            case 3:
-                ocjena = Ocjena.TRI;
-                break;
-            case 4:
-                ocjena = Ocjena.CETIRI;
-                break;
-            case 5:
-                ocjena = Ocjena.PET;
-                break;
-            default:
-                System.out.println("Ova ocjena ne postoji! Pokušajte ponovo!");
-                unosOcjene(scanner);
+            for(Ocjena ocjena : Ocjena.values()) {
+                if(ocjena.getOcjena().compareTo(ocjenaUnos) == 0) {
+                    return ocjena;
+                }
+            }
+        } catch (NumberFormatException ex) {
+            System.out.println("Pogrešan unos, pokušajte ponovo.");
+            return unosOcjene(scanner);
         }
 
-        return ocjena;
+        System.out.println("Pogrešan unos, pokušajte ponovo.");
+        return unosOcjene(scanner);
     }
 }
